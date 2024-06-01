157de484a6d418c57c4f57e0bfaa745c57d32b44
Mine <mine@acon3d.com>
2024-06-01 21:46:53 +0900

    오류 수정

--- app/api/addOwner/route.ts
+++ app/api/addOwner/route.ts
@@ -1,21 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
-
-const supabase = createClient(supabaseUrl, serviceRoleKey);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
+const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY ?? '';
 
 export async function POST(request: NextRequest) {
-  const { id, name, mobile, profile, created_at } = await request.json();
+  try {
+    console.log('start')
+    const supabase = createClient(supabaseUrl, serviceRoleKey);
+    const { name, mobile, profile } = await request.json();
 
-  const { data, error } = await supabase
-    .from('owners') // 테이블 이름
-    .insert([{ id, name, mobile, profile, created_at }]);
-
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    const { data, error } = await supabase
+      .from('owner') // 테이블 이름
+      .insert([{ name, mobile, profile }]);
+  
+    if (error) {
+      console.error(error.code, error.details, error.hint, error.message);
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+  
+    return NextResponse.json(data, { status: 200 });
+  } catch (e) {
+    console.error(e);
   }
-
-  return NextResponse.json(data, { status: 200 });
 }
